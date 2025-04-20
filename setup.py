aa95fdce44c3df118f6daa268f359cf4b5c90978
Eric Langlois <eric@langlois.xyz>
2025-04-19 18:34:16 -0700

    Bump version

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ README = (HERE / "README.md").read_text()
 # This call to setup() does all the work
 setup(
     name="thermal-base",
-    version="1.0.0",
+    version="1.1.0",
     description="Decoding and Basic processing of thermal images",
     long_description=README,
     long_description_content_type="text/markdown",
